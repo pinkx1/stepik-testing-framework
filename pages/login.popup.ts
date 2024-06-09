b0d8d06875bf84901c9cf4d3773690cb63c2b0c6
Julia <shaifelejulia@gmail.com>
2024-06-09 21:46:07 +0400

    added pom for login popup

---
+++ pages/login.popup.ts
@@ -0,0 +1,30 @@
+import { Locator, Page } from '@playwright/test';
+
+export class HomePageUnauth {
+    private page: Page;
+    emailField: Locator;
+    passwordField: Locator;
+    remindPasswordButton: Locator
+    openRegPopupButton: Locator
+    signInButton: Locator;
+    vkButton: Locator;
+    googleButton: Locator
+    githubButton: Locator
+
+    constructor(page: Page) {
+        this.page = page;
+        this.emailField = page.getByPlaceholder('E-mail');
+        this.passwordField = page.getByPlaceholder('Пароль');
+        this.remindPasswordButton = page.getByText('Напомнить пароль');
+        this.openRegPopupButton = page.locator('#ember1845');
+        this.signInButton = page.locator('//button[@type=\'submit\']');
+        this.vkButton = page.locator('button[title=\'Войти через VK\']');
+        this.googleButton = page.locator('button[title=\'Войти через Google\']');
+        this.githubButton = page.locator('button[title=\'Войти через GitHub\']');
+    }
+
+    async goto() {
+        await this.page.goto('/catalog?auth=login');
+    }
+
+}
