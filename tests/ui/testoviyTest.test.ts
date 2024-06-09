e4e3ecd08c60da38b404613ca82e03d1dc581b11
Julia <shaifelejulia@gmail.com>
2024-06-09 21:49:58 +0400

    removed unsusual code

--- tests/ui/testoviyTest.test.ts
+++ tests/ui/testoviyTest.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import {test, expect, Locator} from '@playwright/test';
 import { HomePageUnauth } from '../../pages/home.page.unauth';
 import { RegistrationPopup } from '../../pages/registration.popup';
 import { waitForPopupAndClickLink } from '../../utils/uiHelper';
@@ -7,16 +7,13 @@ test.describe('Registration popup tests', () => {
     let homePage: HomePageUnauth;
     let registrationPopup: RegistrationPopup;
 
+
     test.beforeEach(async ({ page }) => {
         homePage = new HomePageUnauth(page);
         registrationPopup = new RegistrationPopup(page);
     });
 
     test('should open terms link in a new tab', { tag: '@smoke' }, async ({ page }) => {
-        await homePage.goto();
-        await homePage.clickSignUp();
-        await registrationPopup.verifyPopupIsVisible();
-
         const newPage = await waitForPopupAndClickLink(page, registrationPopup.termsLink);
         await expect(newPage).toHaveTitle('Пользовательское соглашение — Stepik');
     });
