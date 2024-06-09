97eedc01775cdb900b0d66200d6c7e87667969a5
Julia <shaifelejulia@gmail.com>
2024-06-09 21:45:22 +0400

    deleted unusual methods

--- pages/home.page.unauth.ts
+++ pages/home.page.unauth.ts
@@ -2,29 +2,12 @@ import { Locator, Page } from '@playwright/test';
 
 export class HomePageUnauth {
     private page: Page;
-    private loginButton: Locator;
-    private signUpButton: Locator;
+    loginButton: Locator;
+    signUpButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.loginButton = page.getByText('Войти', { exact: true });
         this.signUpButton = page.getByText('Регистрация', { exact: true });
-    }
-
-    async goto() {
-        await this.page.goto('/');
-    }
-
-    async clickLogin() {
-        await this.loginButton.click();
-    }
-
-    async clickSignUp() {
-        await this.signUpButton.click();
-    }
-
-    async verifyHomePage() {
-        await this.loginButton.waitFor();
-        await this.signUpButton.waitFor();
     }
 }
